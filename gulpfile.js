8feb36eb2340edd0bf634cf39e2dfe006bb3a089
Rafael <rafael.pol1@upr.edu>
2016-02-27 00:45:34 -0500

    Lots of changes..

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var minify = require('gulp-minify-css');
+
+var dir = {
+	sass: "./src/scss/**/*.scss",
+	js: "./src/js/**/*.js",
+	css: "./src/css/**/*.css",
+}
 
 var sassSrc = "./src/scss/app.scss"
 
-var sassDir = "./src/scss/**/*.scss";
-var jsDir = "./src/js/**/*.js";
-
 // Default Task
-gulp.task('default', ['watch', 'sass', 'babel']);
+gulp.task('default', ['watch', 'sass', 'css-minify', 'babel']);
 
 gulp.task('sass', function() {
-	gulp.src(sassSrc)
+	return gulp.src(sassSrc)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./dist/css'));
 });
 
+gulp.task('css-minify', function() {
+	return gulp.src(dir.css)
+		.pipe(minify())
+		.pipe(gulp.dest('./dist/css'));
+});
+
 gulp.task('babel', function(){
-	return gulp.src(jsDir)
+	return gulp.src(dir.js)
 		.pipe(babel({
 			presets: ['es2015']
 		}))
@@ -27,6 +37,6 @@ gulp.task('babel', function(){
 });
 
 gulp.task('watch', function(){
-	gulp.watch(sassDir, ['sass']);
-	gulp.watch(jsDir, ['babel']);
+	gulp.watch(dir.sass, ['sass']);
+	gulp.watch(dir.js, ['babel']);
 });
